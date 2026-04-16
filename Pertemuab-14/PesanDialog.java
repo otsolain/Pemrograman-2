70be850aa9e0fdfd736be931758130edd34f05f6
Otsolain <otsolain@gmail.com>
2026-04-16 23:00:05 +0700

    Delete Pertemuab-14 directory

--- Pertemuab-14/PesanDialog.java
+++
@@ -1,9 +0,0 @@
-package com.unpam.view;
-
-public class PesanDialog {
-    // Di aplikasi web, konfirmasi dilakukan langsung tanpa dialog
-    // Class ini dibuat agar tidak perlu banyak mengubah kode Model
-    public int showKonfirmasi(String pesan) {
-        return 0; // selalu konfirmasi "Ya"
-    }
-}
